67edebd8bc6f293932e157589c85cbc4802c05fb
Tom Young <tom.young.satx@gmail.com>
2015-07-17 13:55:42 -0500

    using explode, and reading files in php. july17,2015

---
+++ contact-parser.php
@@ -0,0 +1,32 @@
+<?php
+
+$textInput = 'contacts.txt';
+function parseContacts($textInput)
+{
+
+
+    // todo - read file and parse contacts
+    $handle = fopen($textInput, 'r');
+    $contents = trim(fread($handle, filesize($textInput)));
+    $contactsArray = explode(PHP_EOL, $contents);
+    $contacts = array();
+    foreach ($contactsArray as $key => $contact) {
+    	$secondLevelArray['name'] = explode('|', $contact)[0];
+    	$secondLevelArray['number'] = explode('|', $contact)[1];
+    	$secondLevelArray['number'] = formatNumbers($secondLevelArray['number']);
+    	array_push($contacts, $secondLevelArray);
+    }
+    fclose($handle);
+    return $contacts;
+}
+    	
+function formatNumbers ($number) {
+	$number = '(' . substr($number, 0, 3) . ')' . substr($number, 3, 3) . '-' . substr($number, 6, 4);
+	return $number;
+}
+
+
+
+
+
+print_r(parseContacts('contacts.txt'));
---
+++ list_with_commas.php
@@ -0,0 +1,27 @@
+<?php
+ // List of famous peeps
+ function humanizedList($array) {
+ 	// if($sort){
+ 	// 	sort($array);
+ 	// }
+ 	$lastItem = array_pop($array);
+ 	$newString = implode(', ', $array);
+ 	return $newString . ", and " . $lastItem;
+ 	
+   // Your solution goes here!
+
+ }
+ $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+ // TODO: Convert the string into an array
+ $physicistsArray = explode(', ', $physicistsString);
+
+ // Humanize that list
+ $famousFakePhysicists = humanizedList($physicistsArray);
+
+ // Converts array into list n1, n2, ..., and n3
+ 
+ // Output sentence
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
+
+ ?>
